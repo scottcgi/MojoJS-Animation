056e52e738b84e86ec43bf20c6d2fbd0cf1cd109
scottcgi <scott.cgi@gmail.com>
2014-10-11 10:01:47 +0800

    Signed-off-by: scottcgi <scott.cgi@gmail.com>

--- independent/mojoQuery/speedTest/js/frameworks.js
+++ independent/mojoQuery/speedTest/js/frameworks.js
@@ -10,11 +10,11 @@ window.frameworks = [
 
 ["jquery-2.1.1", "http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js", "$"],
 
-["sizzle", "frameworks/sizzle.js", "Sizzle"],
+["sizzle-1.10.19", "http://apps.bdimg.com/libs/sizzle/1.10.19/sizzle.min.js", "Sizzle"],
 
 ["Ext-core-3.1.0", "http://apps.bdimg.com/libs/ext-core/3.1.0/ext-core.js", "Ext.query"],
 
-["MooTools-1.4.5", "http://apps.bdimg.com/libs/mootools/1.4.5/mootools-core-full-compat-yc.js", "$$"],
+["MooTools-1.4.5", "http://apps.bdimg.com/libs/mootools/1.4.5/mootools-yui-compressed.js", "$$"],
 
 ["dojo-1.8.3", "http://apps.bdimg.com/libs/dojo/1.8.3/dojo.js", "dojo.query"]
 

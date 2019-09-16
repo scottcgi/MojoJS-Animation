db25e2781455998ba3d2f7837dd7a9f596eed8c9
scott.cgi <scott.cgi@qq.com>
2019-09-16 15:59:18 +0800

    Update frameworks.js

--- independent/mojoQuery/speedTest/js/frameworks.js
+++ independent/mojoQuery/speedTest/js/frameworks.js
@@ -4,18 +4,10 @@
 window.frameworks = [
 //[iframe name, file name, css query method]
 
-["native", "native", "document.querySelectorAll"],
+    ["native", "native", "document.querySelectorAll"],
 
-["mojoQuery", "../mojoQuery.js", "mojoQuery"],
+    ["mojoQuery", "../mojoQuery.js", "mojoQuery"],
 
-["jquery-2.1.1", "http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js", "$"],
-
-["sizzle-1.10.19", "http://apps.bdimg.com/libs/sizzle/1.10.19/sizzle.min.js", "Sizzle"],
-
-["Ext-core-3.1.0", "http://apps.bdimg.com/libs/ext-core/3.1.0/ext-core.js", "Ext.query"],
-
-["MooTools-1.4.5", "http://apps.bdimg.com/libs/mootools/1.4.5/mootools-yui-compressed.js", "$$"],
-
-["dojo-1.8.3", "http://apps.bdimg.com/libs/dojo/1.8.3/dojo.js", "dojo.query"]
+    ["sizzle-latest", "https://raw.githubusercontent.com/jquery/sizzle/master/src/sizzle.js", "Sizzle"],
 
 ];

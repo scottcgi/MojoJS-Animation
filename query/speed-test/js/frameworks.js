8ddec380a0b7af1f36448c82c522a77b4839facd
scottcgi <scott.cgi@qq.com>
2019-12-07 22:08:48 +0800

    switch dir from upper case to lower case.

---
+++ query/speed-test/js/frameworks.js
@@ -0,0 +1,13 @@
+/**
+ * test frameworks
+ */
+window.frameworks = [
+    //[iframe name, file name, css query method]
+
+    ["native", "native", "document.querySelectorAll"],
+
+    ["MojoJS.query", "../MojoJS.query.js", "MojoJS.query"],
+
+    ["sizzle-mini-v2.3.5", "js/sizzle.min.js", "Sizzle"],
+
+];
